708a7b49e8831fa05857038d95155d7015d96951
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2022-10-17 21:39:33 -0400

    Addition of Indie Author Single Page Site

---
+++ Assets/Javascript/indieAuthorIScript.js
@@ -0,0 +1,199 @@
+//Array holding book information
+const latest = { 
+  entry: 6,
+  title: "Through the Balcony", 
+  subtitle:"What's waiting",
+  blurb: `Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh.
+  <br>
+  Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat.
+  <br>
+  Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+  <br>
+  Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt.
+  `, 
+  image:"images/fantasy-4379818_1920.jpg"
+}
+
+
+const book1 = {
+  entry: 1,
+  title: "Through the Mountain", 
+  subtitle:"What's waiting",
+  blurb: `Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh.
+  <br>
+  Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat.
+  <br>
+  Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+  <br>
+  Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt.
+  `, 
+  image:"images/templar-4298112_1920.jpg"
+  }
+
+const book2 = {
+  entry: 2,
+  title: "Through the Snowy House", 
+  subtitle:"What's waiting",
+  blurb: `Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh.
+  <br>
+  Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat.
+  <br>
+  Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+  <br>
+  Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt.
+  `, 
+  image:"images/night-4028339_1920.jpg"
+}
+
+const book3 = {
+  entry: 3,
+  title: "Through the Sunset", 
+  subtitle:"What's waiting",
+  blurb: `Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh.
+  <br>
+  Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat.
+  <br>
+  Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+  <br>
+  Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt.
+  `, 
+  image:"images/camp-7103189.png"
+}
+
+const book4 = {
+  entry: 4,
+  title: "Through Her Eyes", 
+  subtitle:"What's waiting",
+  blurb: `Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh.
+  <br>
+  Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat.
+  <br>
+  Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+  <br>
+  Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt.
+  `, 
+  image:"images/portrait-4430290_1920.jpg"
+}
+
+const book5 = {
+  entry: 5,
+  title: "Through the Temple", 
+  subtitle:"What's waiting",
+  blurb: `Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh.
+  <br>
+  Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat.
+  <br>
+  Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+  <br>
+  Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt.
+  `, 
+  image:"images/temple-4933682_1920.jpg"
+}
+
+const book6 = {
+  entry: 6,
+  title: "Through the Balcony", 
+  subtitle:"What's waiting",
+  blurb: `Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh.
+  <br>
+  Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat.
+  <br>
+  Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+  <br>
+  Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt.
+  `, 
+  image:"images/fantasy-4379818_1920.jpg"
+}
+
+
+
+
+// LATEST BOOK (HIGHLIGHTED AT TOP OF PAGE)
+document.getElementById('latestTitle').innerHTML = latest.title;
+document.getElementById('latestSubtitle').innerHTML = latest.subtitle;
+document.getElementById('latestBlurb').innerHTML = latest.blurb;
+document.getElementById('latestImage').src = latest.image;
+//function visitPage(){
+//window.open(latest.amazon);
+//}
+
+// BOOK1 THUMBNAIL INFO
+document.getElementById('book1Title').innerHTML = book1.title;
+document.getElementById('book1Image').src = book1.image;
+//function visitPage1(){
+//window.open(latest.amazon);
+//}
+
+// BOOK2 THUMBNAIL INFO
+document.getElementById('book2Title').innerHTML = book2.title;
+document.getElementById('book2Image').src = book2.image;
+//function visitPage2(){
+//window.open(latest.amazon);
+//}
+
+// BOOK3 THUMBNAIL INFO
+document.getElementById('book3Title').innerHTML = book3.title;
+document.getElementById('book3Image').src = book3.image;
+//function visitPage3(){
+//window.open(book3.amazon);
+//}
+
+// BOOK4 THUMBNAIL INFO
+document.getElementById('book4Title').innerHTML = book4.title;
+document.getElementById('book4Image').src = book4.image;
+//function visitPage4(){
+//window.open(book4.amazon);
+//}
+
+// BOOK4 THUMBNAIL INFO
+document.getElementById('book5Title').innerHTML = book5.title;
+document.getElementById('book5Image').src = book5.image;
+//function visitPage5(){
+//window.open(book5.amazon);
+//}
+
+// BOOK4 THUMBNAIL INFO
+document.getElementById('book6Title').innerHTML = book6.title;
+document.getElementById('book6Image').src = book6.image;
+//function visitPage6(){
+//window.open(book6.amazon);
+//}
+
+
+submitForm();
+function submitForm() { // show the form and hide the sign up button.
+  var x = document.getElementById("sentMess");
+  var y = document.getElementById("message");
+  var z = document.getElementById("btnUnStatic");
+  var email = document.getElementById("email");
+  if (x.style.display === "none" && email.innerHTML !== "") {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+const form = document.getElementById('letsTalk');
+
+document.addEventListener('submit', function handleSubmit(event) {
+  // show the form and hide the sign up button.
+    var x = document.getElementById("sentMess");
+    var y = document.getElementById("message");
+    var z = document.getElementById("btnUnStatic");
+    
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+      z.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  event.preventDefault()
+  form.reset()
+});
+
+function fakePage() {
+  alert("If this were a real book, you'd by buying it on Amazon right now. Sorry to disappoint. It's just a button.");
+} 
