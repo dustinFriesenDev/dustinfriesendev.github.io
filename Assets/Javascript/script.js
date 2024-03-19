093d9058fe8a099881fae98233427090aef63f13
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2024-03-18 21:27:02 -0400

    refactored onclick image feature.

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -2,35 +2,7 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-const collectionImages = document.querySelectorAll("img");
-let imgId;
-let tarTagName;
-const imageId = e => {
-    console.log(e.target.id);
-}
-function myTag(event){
-    tarTagName = event.target.tagName;
-}
-
-for (let imageId of collectionImages){
-    imageId.addEventListener("click", imageId)
-}
-
-const sortImages = () => {
-    for (const singleImg of collectionImages){
-        if(singleImg.id == imgId){
-            return singleImg;
-        }
-    }
-}
-
-var img = document.getElementById("img1");
-var modalImg = document.getElementById("modImg");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = img.src;
-}
-
+const modalImg = document.getElementById("modImg");
 const imgParent = document.getElementById("main");
 function clickImg(event){
     if(event.target.tagName == "IMG"){
