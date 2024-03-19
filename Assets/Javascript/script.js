233c4c2ca18ae484a6122b6cf109842983397fc5
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2024-03-19 12:11:17 -0400

    refactoring and editing text and styles

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,22 +1,27 @@
-// Get the modal, image, main
-var modal = document.getElementById("myModal");
+// Get the modal, image, main, body
+const modal = document.getElementById("myModal");
 const modalImg = document.getElementById("modImg");
 const imgParent = document.getElementById("main");
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+const body = document.querySelector("body");
+console.log(body);
+// Insert image inside the modal
 function clickImg(event){
     if(event.target.tagName == "IMG"){
         modal.style.display = "block";
         modalImg.src = event.target.src;
+        body.style.overflow = "hidden";
     } else {
         return null;
     }
 }
+// Listener to get image clicked
 imgParent.addEventListener("click", clickImg);
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+// When modal is clicked image closes
+modal.onclick = () => {
   modal.style.display = "none";
+  body.style.overflow = "visible";
 }
